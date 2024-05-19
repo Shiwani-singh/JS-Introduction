f0e6545cf96a4a3719dfd432c363fc5eadb2c8b1
shivani <codershivani@gmail.com>
2024-05-19 14:09:54 +0530

    added functionality of increment and decrement of number for buttons

---
+++ script.js
@@ -0,0 +1,16 @@
+// console.log(document)
+let btn1Element = document.getElementById("btn1");
+let valueElement = document.getElementById("value");
+let btn2Element = document.getElementById("btn2");
+console.log(btn1Element, valueElement, btn2Element);
+let value = "0";
+valueElement.innerText = value;
+btn1Element.addEventListener("click", () => {
+  value += 1;
+  valueElement.innerText = value;
+});
+
+btn2Element.addEventListener("click", () => {
+    value -= 1;
+    valueElement.innerText = value;
+  });
